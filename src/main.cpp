4aa57389044c52b5b2b05fd377a16295d98fd702
JitChowDhury <jitchowdhury8717@gmail.com>
2025-10-27 11:33:45 +0530

    Updated Cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,15 @@
 #include "raylib.h"
 #include "raymath.h"
 
-
 int main()
 {
-	const int windowWidth{ 384 };
-	const int windowHeight{ 384 };
+	const int windowWidth{384};
+	const int windowHeight{384};
 	InitWindow(windowWidth, windowHeight, "OutRun");
 
 	Texture2D map = LoadTexture("assets/tilesets/worldmap.png");
-	Vector2 mapPos{ 0.0,0.0 };
-	float speed{ 4.0 };
+	Vector2 mapPos{0.0, 0.0};
+	float speed{4.0};
 
 	SetTargetFPS(60);
 	while (!WindowShouldClose())
@@ -19,24 +18,24 @@ int main()
 		ClearBackground(WHITE);
 		Vector2 direction{};
 
-		if (IsKeyDown(KEY_A)) direction.x -= 1.0;
-		if (IsKeyDown(KEY_D)) direction.x += 1.0;
-		if (IsKeyDown(KEY_W)) direction.y -= 1.0;
-		if (IsKeyDown(KEY_S)) direction.y += 1.0;
+		if (IsKeyDown(KEY_A))
+			direction.x -= 1.0;
+		if (IsKeyDown(KEY_D))
+			direction.x += 1.0;
+		if (IsKeyDown(KEY_W))
+			direction.y -= 1.0;
+		if (IsKeyDown(KEY_S))
+			direction.y += 1.0;
 
 		if (Vector2Length(direction) != 0.0)
 		{
-			//set map pos = mappos-direction
-			 
-			mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction),speed));
-		    
-		}
+			// set map pos = mappos-direction
 
+			mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+		}
 
 		DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 		EndDrawing();
 	}
 	CloseWindow();
-
-
 }
